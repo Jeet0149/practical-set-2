1aafedba4309489632add3f92e9e901336faf151
Jeet0149 <110480888+Jeet0149@users.noreply.github.com>
2022-08-06 22:03:59 +0530

    Add files via upload

---
+++ practical_5.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class Additions{
+    public int add(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+    public int add(int a, int b,int c)
+    {
+        int mul=a*b*c;
+        return mul;
+    }
+}
+public class practical_5 {
+    public static void main(String[] args) {
+        Additions A = new Additions();
+
+        System.out.println(" METHOD 1 : "+A.add(1,2));
+        System.out.println(" METHOD 2 : "+A.add(1,2,3));
+        System.out.println("21CE149-Jeet");
+
+
+    }
+}
